f172eeb01852d956a417f92ef27e3757f023f38e
Andrey <101170296+andreymalakanov@users.noreply.github.com>
2022-05-18 21:01:42 +0000

    7.8

--- Task_7.07.py
+++ Task_7.07.py
@@ -8,4 +8,18 @@ print('Задача 7. Отрезок')
 # считает и выводит на консоль 
 #среднее арифметическое всех чисел из отрезка [a; b], которые кратны числу 3.
 
-stunets_num = int(input('Введите число: '))
+a = int(input('Введите число a: '))
+b = int(input('Введите число b: '))
+counter = 0
+counter_sum = 0
+if a > b:
+  a, b = b, a
+for number in range (a, b + 1):
+  if number % 3 == 0:
+    counter += 1
+    counter_sum += number
+#    print(counter, counter_sum)
+if counter >= 1:
+  print('Ответ:', counter_sum / counter)
+else:
+  print('Указан слишком короткий диапазон')
